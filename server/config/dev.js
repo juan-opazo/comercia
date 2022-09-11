83ce6759875dfe08de1a5f23cbb9ea3b7a81071d
Juan Opazo <dopazo178@gmail.com>
2022-09-10 20:16:24 -0500

    temporary add dev.js

---
+++ server/config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleClientID: '59821523779-94fbg14eebnh4pt04dgla5qp87s5oqm7.apps.googleusercontent.com',
+    googleClientSecret: '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',
+    mongoURI: 'mongodb+srv://comercia:holamundoestoescomercia1@cluster0.4mh6zcu.mongodb.net/?retryWrites=true&w=majority',
+    cookieKey: 'oahrgiuwrnipjnaprogihsdpfinbwjibvdiusbvpsodnjvipdjsnbvibubt'
+}
