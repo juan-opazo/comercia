4d69228b08826681f29b63540cb8bbb40662616d
Juan Opazo <dopazo178@gmail.com>
2022-10-03 22:06:59 -0500

    delete dev and update prod credential

---
+++ server/config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleClientID: process.env.GOOGLE_CLIENT_ID,//'648112998164-2ovbjgnmik4g34cg004bgn1bmioluo4f.apps.googleusercontent.com', //process.env.GOOGLE_CLIENT_ID
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,//'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',
+    mongoURI: process.env.MONGO_URI,//'mongodb+srv://admincomercia:eYpoxwa4MY21pb9L@cluster0.k8gyhhn.mongodb.net/?retryWrites=true&w=majority',
+    cookieKey: process.env.COOKIE_KEY//'oahrgiuwrrogihsdpfinbwjibvdiusbvpsodnjvipdjsnbvibubt'
+}
